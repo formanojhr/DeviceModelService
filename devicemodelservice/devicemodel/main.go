7552170fb6e35db2e1c11ee1c7e055ac585741df
manoj.ramakrishnan@poly.com <manoj.ramakrishnan@poly.com>
2020-09-02 13:11:13 -0700

    adding support for put and post
    Signed-off-by: manoj.ramakrishnan@poly.com <manoj.ramakrishnan@poly.com>

--- devicemodelservice/devicemodel/main.go
+++ devicemodelservice/devicemodel/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	deviceapi "DeviceModelService/devicemodelservice/api"
+	model2 "DeviceModelService/devicemodelservice/domain/model"
 	"DeviceModelService/devicemodelservice/registry"
 	"encoding/json"
 	"fmt"
@@ -15,6 +16,7 @@ import (
 
 var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
+// A Serverless lambda entry point for DeviceModel Service
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
 //
@@ -59,26 +61,21 @@ func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, 
 	switch req.HTTPMethod {
 	case "GET":
 		return get(api, req)
-	//case "POST":
-	//	return create(req)
+	case "PUT":
+		return updateDeviceModel(api, req)
+	case "POST":
+		return createModel(api, req)
 	default:
 		return clientError(http.StatusMethodNotAllowed)
 	}
 }
 
-// create device model API container
-func createDeviceModelAPIInstance() *registry.DeviceModelContainer {
-	ctn, err := registry.NewContainer()
-	if err != nil {
-		return ctn
-	}
-	return ctn
-}
-
 // process HTTP Get request
 func get(api *deviceapi.DeviceModelAPIInstance, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	rawName, found := req.QueryStringParameters["name"]
+
 	if found {
+		fmt.Printf("Getting model metadata for %s \n", rawName)
 		// query parameters are typically URL encoded so to get the value
 		name, err := url.QueryUnescape(rawName)
 		if err != nil {
@@ -110,6 +107,68 @@ func get(api *deviceapi.DeviceModelAPIInstance, req events.APIGatewayProxyReques
 			Body:       string(js),
 		}, nil
 	}
+}
+
+func createModel(api *deviceapi.DeviceModelAPIInstance, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	model := new(model2.DeviceModel)
+	err := json.Unmarshal([]byte(req.Body), model) // unmarshall body into device model
+	if err != nil {
+		return clientError(http.StatusUnprocessableEntity)
+	}
+	fmt.Printf("Adding model name %s \n", model.ModelName)
+	if model.ModelName == "" || model.DeviceType == "" {
+		return clientError(http.StatusBadRequest)
+	}
+	modelExists, err := api.Service.GetModelByName(model.ModelName)
+	if err != nil {
+		return serverError(err)
+	}
+	if modelExists != nil {
+		return clientError(http.StatusConflict)
+	}
+	createErr, typeUUID := api.Service.AddModel(*model)
+
+	if createErr != nil {
+		return serverError(createErr)
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers:    map[string]string{"Location": fmt.Sprintf("/models?typeUUID=%s", typeUUID)},
+	}, nil
+
+}
+
+func updateDeviceModel(api *deviceapi.DeviceModelAPIInstance, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	model := new(model2.DeviceModel)
+	err := json.Unmarshal([]byte(req.Body), model) // unmarshall body into device model
+	if err != nil {
+		return clientError(http.StatusUnprocessableEntity)
+	}
+	fmt.Printf("Updating model name %s \n", model.ModelName)
+	modelExists, err := api.Service.GetModelByName(model.ModelName)
+	if err != nil {
+		return serverError(err)
+	}
+	if modelExists == nil {
+		return clientError(http.StatusBadRequest)
+	}
+	err = api.Service.UpdateModel(*model)
+	if err != nil {
+		return serverError(err)
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       model.ModelName + "Updated",
+	}, nil
+}
+
+// create device model API container
+func createDeviceModelAPIInstance() *registry.DeviceModelContainer {
+	ctn, err := registry.NewContainer()
+	if err != nil {
+		return ctn
+	}
+	return ctn
 }
 
 // Helper to convert server error into APIGateway error response
