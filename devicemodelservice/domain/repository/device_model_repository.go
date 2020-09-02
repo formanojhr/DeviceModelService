193c2cc8f558636db670e36b7fca8df08c0784c5
manoj.ramakrishnan@poly.com <manoj.ramakrishnan@poly.com>
2020-09-02 13:47:55 -0700

    change in FindByModelName interface. Adding support for Update delete etc
    Signed-off-by: manoj.ramakrishnan@poly.com <manoj.ramakrishnan@poly.com>

--- devicemodelservice/domain/repository/device_model_repository.go
+++ devicemodelservice/domain/repository/device_model_repository.go
@@ -7,6 +7,8 @@ import (
 //Repository interface
 type DeviceModelRepository interface {
 	FindAll() ([]*model.DeviceModel, error)
-	FindByModelName(modeName string) (*model.DeviceModel, error)
-	Save(*model.DeviceModel) error
+	FindByModelName(modelName string) (*model.DeviceModel, error)
+	Save(model *model.DeviceModel) (error, model.DeviceModel) // TODO remove pointers pass be reference for struct; Makes it more confusin
+	UpdateModel(modelUpdate model.DeviceModel, oldModel model.DeviceModel) error
+	DeleteItem(modelName string) (error, string)
 }
